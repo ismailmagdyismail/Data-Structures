3bee6097203d8add3279559bf96e841da175a2c9
ismail <ismailmagdy88@gmail.com>
2023-08-23 03:32:11 +0300

    Added vector-based Stack and test cases

--- src/main.go
+++ src/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"GO/src/linkedList"
+	"GO/src/stack"
+	"fmt"
 )
 
 // Your doublyLinkedList implementation here...
@@ -20,12 +22,29 @@ func testLinkedList(list linkedList.LinkedList) {
 	list.RemoveFront()
 	list.Print() // Should print: 2 3 4 5
 }
+func testStack(stack stack.Stack) {
 
+	fmt.Println("Is the stack empty?", stack.IsEmpty())
+
+	stack.Push(42)
+	stack.Push(73)
+	stack.Push(15)
+
+	fmt.Println("Stack size:", stack.Size())
+	fmt.Println("Top element:", stack.Top())
+
+	fmt.Println("Popping elements:")
+	for !stack.IsEmpty() {
+		fmt.Println("Popped:", stack.Top())
+		stack.Pop()
+	}
+
+	fmt.Println("Is the stack empty?", stack.IsEmpty())
+}
 func main() {
 
 	// Create a new doubly linked list
-	var doubly linkedList.LinkedList = linkedList.NewDoublyLinkedList()
-	var singly linkedList.LinkedList = linkedList.NewSinglyLinkedList()
-	testLinkedList(doubly)
-	testLinkedList(singly)
+	//testLinkedList(linkedList.NewDoublyLinkedList())
+	//testLinkedList(linkedList.NewSinglyLinkedList())
+	testStack(stack.NewStackDynamic())
 }
---
+++ src/stack/StackDynamic.go
@@ -0,0 +1,40 @@
+package stack
+
+import "fmt"
+
+type stackDynamic struct {
+	size  int
+	stack []any
+}
+
+func NewStackDynamic() Stack {
+	return &stackDynamic{
+		size:  0,
+		stack: make([]any, 0),
+	}
+}
+
+func (stack *stackDynamic) IsEmpty() bool {
+	return stack.Size() == 0
+}
+func (stack *stackDynamic) Size() int {
+	return stack.size
+}
+func (stack *stackDynamic) Top() any {
+	if stack.IsEmpty() {
+		panic("Stack is Empty , No elements Exist")
+	}
+	return stack.stack[stack.size-1]
+}
+func (stack *stackDynamic) Push(element any) {
+	stack.stack = append(stack.stack, element)
+	stack.size++
+}
+func (stack *stackDynamic) Pop() {
+	if stack.IsEmpty() {
+		panic("Stack is already Empty ,cannot POP")
+	}
+	stack.stack = stack.stack[:stack.size-1]
+	stack.size--
+	fmt.Println(stack)
+}
